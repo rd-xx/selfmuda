742d860c61339f6dc02767972e9183de6f80a8e6
rdx <31172722+rd-xx@users.noreply.github.com>
2022-10-13 13:34:43 +0200

    load events manually

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Client } from 'discord.js-selfbot-v13';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+require('dotenv').config();
+
+const client = new Client({
+  intents: ['GUILDS', 'GUILD_MESSAGES', 'MESSAGE_CONTENT'],
+  checkUpdate: false,
+});
+
+async function main() {
+  // load events
+  const eventsPath = join(__dirname, 'events');
+  const eventFiles = readdirSync(eventsPath).filter((file) =>
+    file.endsWith('.js')
+  );
+
+  for (const file of eventFiles) {
+    const filePath = join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+  }
+
+  await client.login();
+}
+
+main();
